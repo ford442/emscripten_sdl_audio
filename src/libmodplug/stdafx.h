6b30e56391338de239da418dc53b7ffdc4a28fef
Noah Wolfe Cohn <noahc42@gmail.com>
2021-08-01 18:34:00 -0700

    edit

--- src/libmodplug/stdafx.h
+++ src/libmodplug/stdafx.h
@@ -1,15 +1,6 @@
-/*
- * This source code is public domain.
- *
- * Authors: Rani Assaf <rani@magic.metawire.com>,
- *          Olivier Lapicque <olivierl@jps.net>,
- *          Adam Goode       <adam@evdebs.org> (endian and char fixes for PPC)
- */
 
 #ifndef _STDAFX_H_
 #define _STDAFX_H_
-
-/* Autoconf detection of stdint/inttypes */
 #if defined(HAVE_CONFIG_H) && !defined(CONFIG_H_INCLUDED)
 # include "config.h"
 # define CONFIG_H_INCLUDED 1
@@ -20,8 +11,6 @@
 #ifdef HAVE_STDINT_H
 # include <stdint.h>
 #endif
-
-
 #ifdef _WIN32
 
 #ifdef MSC_VER
@@ -43,44 +32,37 @@
 inline void ProcessPlugins(int n) {}
 
 #else
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #ifdef HAVE_MALLOC_H
 #include <malloc.h>
 #endif
-
 #include <stdint.h>
-
 typedef int8_t CHAR;
 typedef uint8_t UCHAR;
-typedef uint8_t* PUCHAR;
+typedef uint8_t *PUCHAR;
 typedef uint16_t USHORT;
 typedef uint32_t ULONG;
 typedef uint32_t UINT;
 typedef uint32_t DWORD;
 typedef int32_t LONG;
 typedef int64_t LONGLONG;
-typedef int32_t* LPLONG;
-typedef uint32_t* LPDWORD;
+typedef int32_t *LPLONG;
+typedef uint32_t *LPDWORD;
 typedef uint16_t WORD;
 typedef uint8_t BYTE;
-typedef uint8_t* LPBYTE;
+typedef uint8_t *LPBYTE;
 typedef bool BOOL;
-typedef char* LPSTR;
-typedef void* LPVOID;
-typedef uint16_t* LPWORD;
-typedef const char* LPCSTR;
-typedef void* PVOID;
+typedef char *LPSTR;
+typedef void *LPVOID;
+typedef uint16_t *LPWORD;
+typedef const char *LPCSTR;
+typedef void *PVOID;
 typedef void VOID;
-
-inline LONG MulDiv (long a, long b, long c)
-{
-  // if (!c) return 0;
-  return ((uint64_t) a * (uint64_t) b ) / c;
+inline LONG MulDiv(long a,long b,long c){
+return ((uint64_t) a * (uint64_t) b) / c;
 }
-
 #define MODPLUG_NO_FILESAVE
 #define NO_AGC
 #define LPCTSTR LPCSTR
@@ -88,39 +70,26 @@ inline LONG MulDiv (long a, long b, long c)
 #define lstrcpy strcpy
 #define lstrcmp strcmp
 #define WAVE_FORMAT_PCM 1
-//#define ENABLE_EQ
-
 #define  GHND   0
-
-inline int8_t * GlobalAllocPtr(unsigned int, size_t size)
-{
-  int8_t * p = (int8_t *) malloc(size);
-
-  if (p != NULL) memset(p, 0, size);
-  return p;
+inline int8_t *GlobalAllocPtr(unsigned int,size_t size){
+int8_t *p=(int8_t *) malloc(size);
+if(p != NULL) memset(p,0,size);
+return p;
 }
-
-inline void ProcessPlugins(int n) {}
-
+inline void ProcessPlugins(int n){}
 #define GlobalFreePtr(p) free((void *)(p))
-
 #ifndef strnicmp
-#define strnicmp(a,b,c)		strncasecmp(a,b,c)
+#define strnicmp(a,b,c)        strncasecmp(a,b,c)
 #endif
-
-#define wsprintf			sprintf
+#define wsprintf            sprintf
 #define HAVE_SINF 1
-
 #ifndef FALSE
-#define FALSE	false
+#define FALSE    false
 #endif
-
 #ifndef TRUE
-#define TRUE	true
+#define TRUE    true
 #endif
-
 #endif // _WIN32
-
 #endif
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,106 +1,88 @@
-// Vanilla Linux OpenGL+GLUT+SOIL App
-
 #include <stdio.h>
 #include <dirent.h>
 #include <vector>
-
-
 #include "modplug.h"
 #include "SDL.h"
 #include "SDL_audio.h"
-
-
 static std::vector<ModPlugFile *> m_Sounds;
-static int m_CurrentSound = 0;
-
-
-void mixaudio(void *unused, Uint8 *stream, int len) {
-  //printf("short=%d, Uint8=%d len=%d\n", sizeof(short), sizeof(Uint8), len);
-  memset(stream, 0, len);
-  int read = ModPlug_Read(m_Sounds[m_CurrentSound], stream, len);
-  if (read == 0) {
-    ModPlug_Seek(m_Sounds[m_CurrentSound], 0);
-  }
+static int m_CurrentSound=0;
+void mixaudio(void *unused,Uint8 *stream,int len){
+//printf("short=%d, Uint8=%d len=%d\n", sizeof(short), sizeof(Uint8), len);
+memset(stream,0,len);
+int read=ModPlug_Read(m_Sounds[m_CurrentSound],stream,len);
+if(read == 0){
+ModPlug_Seek(m_Sounds[m_CurrentSound],0);
+}
+}
+char *path_cat(const char *str1,char *str2){
+size_t str1_len=strlen(str1);
+size_t str2_len=strlen(str2);
+char *result;
+result=(char *) malloc((str1_len+str2_len+1) * sizeof *result);
+strcpy(result,str1);
+int i,j;
+for (i=str1_len,j=0; ((i < (str1_len+str2_len)) && (j < str2_len)); i++,j++){
+result[i]=str2[j];
+}
+result[str1_len+str2_len]='\0';
+return result;
+}
+int is_not_dot_or_dot_dot(const struct dirent *dp){
+if(strcmp(".",dp->d_name) == 0 || strcmp("..",dp->d_name) == 0){
+return 0;
+} else{
+return 1;
+}
+}
+int alphasort(const struct dirent **a,const struct dirent **b){
+return strcmp((*a)->d_name,(*b)->d_name);
+}
+int main(int argc,char **argv){
+struct dirent *dp;
+struct dirent **dps;
+const char *dir_path="/assets/sounds/";
+DIR *dir=opendir(dir_path);
+if(dir){
+while ((dp=readdir(dir)) != NULL){
+char *tmp;
+tmp=path_cat(dir_path,dp->d_name);
+if(strcmp(".",dp->d_name) == 0 || strcmp("..",dp->d_name) == 0){
+} else{
+FILE *fd=fopen(tmp,"rb");
+fseek(fd,0,SEEK_END);
+unsigned int len=ftell(fd);
+rewind(fd);
+void *buffer=(void *) malloc(sizeof(char) * len);
+fseek(fd,0,SEEK_SET);
+size_t r=fread(buffer,1,len,fd);
+if(r > 0){
+m_Sounds.push_back(ModPlug_Load(buffer,len));
+}
+free(buffer);
+}
+free(tmp);
+tmp=NULL;
+}
+closedir(dir);
 }
 
+// Set 16-bit stereo audio at 44.1Khz
+SDL_AudioSpec fmt;
+fmt.freq=44100;
+fmt.format=AUDIO_S16;
+fmt.channels=2;
+fmt.samples=(int) ((1.0 / 35.0) * ((float) fmt.freq / (float) sizeof(short)));
+printf("%d\n",fmt.samples);
+fmt.callback=mixaudio;
+fmt.userdata=NULL;
 
-char *path_cat (const char *str1, char *str2) {
-	size_t str1_len = strlen(str1);
-	size_t str2_len = strlen(str2);
-	char *result;
-	result = (char*)malloc((str1_len+str2_len+1)*sizeof *result);
-	strcpy (result,str1);
-	int i,j;
-	for(i=str1_len, j=0; ((i<(str1_len+str2_len)) && (j<str2_len));i++, j++) {
-		result[i]=str2[j];
-	}
-	result[str1_len+str2_len]='\0';
-	return result;
+// Open the audio device and start playing sound!
+if(SDL_OpenAudio(&fmt,NULL) < 0){
+printf("No audio: %s\n",SDL_GetError());
+} else{
+SDL_PauseAudio(0);
 }
+throw 0; //crashes the main loop and allows the callbacks to proceed, kinda ugly
 
-
-int is_not_dot_or_dot_dot(const struct dirent *dp) {
-  if (strcmp(".", dp->d_name) == 0 || strcmp("..", dp->d_name) == 0) {
-    return 0;
-  } else {
-    return 1;
-  }
-}
-
-
-int alphasort(const struct dirent **a, const struct dirent **b) {
-  return strcmp((*a)->d_name,(*b)->d_name);
-}
-
-
-int main(int argc, char** argv) {
-  struct dirent *dp;
-  struct dirent **dps;
-  const char *dir_path="/assets/sounds/";
-  DIR *dir = opendir(dir_path);
-  if (dir) {
-    while ((dp=readdir(dir)) != NULL) {
-      char *tmp;
-      tmp = path_cat(dir_path, dp->d_name);
-      if (strcmp(".", dp->d_name) == 0 || strcmp("..", dp->d_name) == 0) {
-      } else {
-        FILE *fd = fopen(tmp, "rb");
-        fseek(fd, 0, SEEK_END);
-        unsigned int len = ftell(fd);
-        rewind(fd);
-        void *buffer = (void *)malloc(sizeof(char) * len);
-        fseek(fd, 0, SEEK_SET);
-        size_t r = fread(buffer, 1, len, fd);
-        if (r > 0) {
-          m_Sounds.push_back(ModPlug_Load(buffer, len));
-        }
-        free(buffer);
-      }
-    
-      free(tmp);
-      tmp=NULL;
-    }
-    closedir(dir);
-  }
-
-  // Set 16-bit stereo audio at 44.1Khz
-  SDL_AudioSpec fmt;
-  fmt.freq = 44100;
-  fmt.format = AUDIO_S16;
-  fmt.channels = 2;
-  fmt.samples = (int)((1.0 / 35.0) * ((float)fmt.freq / (float)sizeof(short)));
-  printf("%d\n", fmt.samples);
-  fmt.callback = mixaudio;
-  fmt.userdata = NULL;
-
-  // Open the audio device and start playing sound!
-  if (SDL_OpenAudio(&fmt, NULL) < 0) {
-    printf("No audio: %s\n", SDL_GetError());
-  } else {
-    SDL_PauseAudio(0);
-  }
-
-  throw 0; //crashes the main loop and allows the callbacks to proceed, kinda ugly
-
-  return 0;
+return 0;
 }
